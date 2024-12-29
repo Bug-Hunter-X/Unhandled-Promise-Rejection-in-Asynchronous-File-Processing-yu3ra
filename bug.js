eddf40cede21ca2696b43b3c44b2b65f71b3071a
BugHunter-X <askzenai@gmail.com>
2024-12-29 02:26:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,26 @@
+```javascript
+const fs = require('fs');
+
+function processFile(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+async function main() {
+  try {
+    const content = await processFile('./my-file.txt');
+    console.log(content);
+  } catch (error) {
+    console.error('Error processing file:', error);
+  }
+}
+
+main();
+```
